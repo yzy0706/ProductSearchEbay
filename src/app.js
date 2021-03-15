e550e17ce95f97aa2c816ff57e542f1f50dd9d94
Zhongyu <yizhongyu2014@gmail.com>
2021-03-14 22:37:46 -0700

    Mar 14

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 const http = require('http');
-
+const express = require('express');
 const hostname = '127.0.0.1'
 const port = 3000;
+const cors = require('cors')
+// const url = require("buildURL");
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-    console.log('Server running at http://${hostname}:${port}/');
-});
+const app = express();
+app.use(cors())
+app.get("/test", function(req, res){
+    console.log("Test");
+    // const address = url.buildSearchUrl(options);
+    // console.log(req);
+    res.json("hello");
+})
+
+app.listen(port, function(){
+    console.log("Test2");
+})
+
+// server.listen(port, hostname, () => {
+//     console.log('Server running at http://${hostname}:${port}/');
+// });
